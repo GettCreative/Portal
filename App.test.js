700696f6d8ce65adea0ff318b3eddb3e344ceab1
MohammadAlRifai <mohammad94sar@gmail.com>
2018-05-13 17:06:41 +0300

    Add Login Test

--- App.test.js
+++ App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import App from './App';
+import 'react-native';
+import Login from './app/components/Login.js';
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
+});
+
+it('Login renders correctly', () => {
+  const tree = renderer.create(
+    <Login />
+    ).toJSON();
+  expect(tree).toBeTruthy();
 });
